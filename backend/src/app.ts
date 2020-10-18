947164defb00d147d5c6c50626af77cffa65086f
SAFA <sammoudasaffa@gmail.com>
2020-10-17 22:34:02 -0400

    ap.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,31 @@
 import bodyParser from 'body-parser';
 import errorHandler from 'errorhandler';
 import express from 'express';
-import { authRouter } from './router/authrouter';
+import MySQLStore from 'express-mysql-session';
+import session from 'express-session';
+import passport from 'passport';
+import { Strategy } from 'passport-local';
+import { config } from './config';
+import { authRouter, loginHandler } from './router/authrouter';
 import { commentRouter } from './router/commentrouter';
 
+const sessionStore = new (MySQLStore(session as any))({
+    host: config.database.url,
+    user: config.database.username,
+    password: config.database.password,
+    database: config.database.database + '_session'
+});
+
 const app = express();
+app.use(session({
+    name: 'archetype_session',
+    secret: '',
+    store: sessionStore,
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.set('trust proxy', 'loopback');
 app.use(errorHandler({ log: true }));
@@ -17,10 +38,14 @@ app.use((_req, res, next) => {
     res.header('Pragma', 'no-cache');
     next();
 });
+passport.deserializeUser((user, done) => {
+    done(null, user);
+});
+passport.serializeUser((user, done) => {
+    done(null, user);
+});
+passport.use(new Strategy(loginHandler));
 
-// app.get('/', (_req, res) => {
-//     res.send('Allo');
-// });
 
 app.use('/comment', commentRouter);
 app.use('/auth', authRouter);
--- backend/src/router/authrouter.ts
+++ backend/src/router/authrouter.ts
@@ -1,20 +1,44 @@
+// import { wrap } from '../util';
 import bcrypt from 'bcrypt';
 import { Router } from 'express';
-import { wrap } from '../util';
-const authRouter = Router();
+import passport from 'passport';
+import { DBProvider } from '../dbprovider';
 
-const users = [{ username: 'safa', password: '$2b$10$ZtZko2qKObAfLQ17eapOseN9gQRJhQY/vcNwNx.7Onyei/lJ7i0Ua' }];
+
+const authRouter = Router();
+const knex = DBProvider.getKnexConnection();
+
+// const users = [{ username: 'safa', password: '$2b$10$ZtZko2qKObAfLQ17eapOseN9gQRJhQY/vcNwNx.7Onyei/lJ7i0Ua' }];
 // const slatRounds = 10;
 
-authRouter.get('/login', wrap(async (req, res) => {
-    const username = req.query.username as string;
-    const password = req.query.password as string;
-    const user = users.find(userBeingSearched => userBeingSearched.username === username);
-    const matches = await bcrypt.compare(password, user?.password ?? '');
-    if (!matches) { return res.sendStatus(401); }
+authRouter.get('/login', passport.authenticate('local', { session: true }), (req, res) => {
+    // const username = req.query.username as string;
+    // const password = req.query.password as string;
+    // const user = users.find(userBeingSearched => userBeingSearched.username === username);
+    // const matches = await bcrypt.compare(password, user?.password ?? '');
+    // if (!matches) { return res.sendStatus(401); }
     // const hash = await bcrypt.hash(password, slatRounds);
     // console.log(`${hash}`);
+    if (req.user) {
+        res.send();
+    } else {
+        res.sendStatus(401);
+    }
     return res.send();
-}));
+});
+
+const loginHandler = async (username: string, password: string, done: (error: any, user?: any) => void) => {
+    const user = await knex('user').first('userId', 'username', 'password').where({ username });
+    if (user === undefined) {
+        return done(null, false);
+    }
+    if (await bcrypt.compare(password, user.password)) {
+        delete user.password;
+        return done(null, user);
+    }
+    return done(null, false);
+
+};
 
 export { authRouter };
+export { loginHandler };
